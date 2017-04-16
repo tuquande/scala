0b349db25dbc48e3e232038b568b6e7e4a236efd
tuquande <IT@DESKTOP-72ET21A.vanke.net.cn>
2017-04-16 17:25:20 +0800

    1

--- src/FileHandler/package.scala
+++ src/FileHandler/package.scala
@@ -1,10 +1,25 @@
 
-
-
+import scala.io._
+import java.io._
 package object FileHandler {
-  val file = "filehandler"
- def readFile(fileName:String) ={
-   
- }
+
+  var token: List[String] = List()
+
+  def readFile(fileName: String) = {
+    val source = Source.fromFile(fileName)
+    val contents = source.mkString
+    val words = contents.split("\\s+").toList //按照空格拆分成多个单词
+
+    println(contents)
+    println(words)
+    source.close()
+    token = words
+  }
+
+  def writeFile(fileName: String) = {
+    val writer = new PrintWriter(fileName)
+    writer.println(token)
+    writer.close()
+  }
 }
 
