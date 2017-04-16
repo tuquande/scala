ed8afc36cf3eb27936d7a213003aba1428f55c41
tuquande <IT@DESKTOP-72ET21A.vanke.net.cn>
2017-04-16 16:02:55 +0800

    1.0

--- src/FileHandler.scala
+++ src/FileHandler.scala
@@ -1,5 +1,12 @@
+//import FileHandler._
 
-
-object FileHandler {
+object FileHandler1 {
   
+}
+
+object app extends App{
+  val fileName = "c:\\scala\future.txt"
+
+ FileHandler.readFile(fileName)
+ 
 }
